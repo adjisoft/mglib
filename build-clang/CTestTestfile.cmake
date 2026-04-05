e43c7efe74990024f9b7725f83b80fece2e5c622
adjisoft <creeperindoors@gmail.com>
2026-04-05 13:46:58 +0700

    update

--- build-clang/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chi/repos/magercode/mglib
-# Build directory: /home/chi/repos/magercode/mglib/build-clang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mglib_tests "/home/chi/repos/magercode/mglib/build-clang/mglib_tests")
-set_tests_properties(mglib_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/chi/repos/magercode/mglib/CMakeLists.txt;19;add_test;/home/chi/repos/magercode/mglib/CMakeLists.txt;0;")
